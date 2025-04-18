97c16f78a2ceb4663287e43f79f98102f35a3fc6
TrRollet <tristan.rollet38@gmail.com>
2025-04-18 09:17:07 +0200

    Avancée sur SAT (ne marche pas encore) et fin du Sokoban

--- Sokoban/target/classes/view/config.js
+++
@@ -1,7 +0,0 @@
-import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
-import { TooltipModule } from './tooltip-module/TooltipModule.js';
-
-export const modules = [
-	GraphicEntityModule,
-	TooltipModule
-];
